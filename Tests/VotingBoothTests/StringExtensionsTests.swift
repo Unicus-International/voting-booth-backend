695796af8dbff5c7bc070755ddb9d6a77f6875db
Williham Totland <williham.totland@gmail.com>
2019-10-07 08:42:59 +0200

    Reframes the test failure reasons of StringExtensionTests in terms of how the assertion failed.

--- Tests/VotingBoothTests/StringExtensionsTests.swift
+++ Tests/VotingBoothTests/StringExtensionsTests.swift
@@ -4,29 +4,30 @@ import VotingBooth
 final class StringExtensionsTests: XCTestCase {
 
   func testValidEmails() {
-    XCTAssertTrue("testuser@unicus.no".isLikelyEmail, "Trivial case.")
-    XCTAssertTrue("a@a.aa".isLikelyEmail, "Minimal case.")
-    XCTAssertTrue("user@some-server.com".isLikelyEmail, "Domain contains a dash.")
-    XCTAssertTrue("some-user@domain.com".isLikelyEmail, "Username contains a dash.")
-    XCTAssertTrue("user.name+voting-booth@gmail.com".isLikelyEmail, "Username contains plussed component.")
-    XCTAssertTrue("user@strange.top.level.domain".isLikelyEmail, "Non-.com-domain.")
-    XCTAssertTrue("user@normal.domain.com".isLikelyEmail, "Multiply dotted domain.")
-    XCTAssertTrue("user123456789@domain5.com".isLikelyEmail, "Alphanumeric username.")
-    XCTAssertTrue("004798765432@telia.no".isLikelyEmail, "Telephone number-like username.")
+    XCTAssertTrue("testuser@unicus.no".isLikelyEmail, "Trivial case rejected.")
+    XCTAssertTrue("a@a.aa".isLikelyEmail, "Minimal case rejected.")
+    XCTAssertTrue("user@some-server.com".isLikelyEmail, "Domain containing dash rejected.")
+    XCTAssertTrue("some-user@domain.com".isLikelyEmail, "Username containing dash rejected.")
+    XCTAssertTrue("user.name+voting-booth@gmail.com".isLikelyEmail, "Username containing plussed component rejected.")
+    XCTAssertTrue("user@normal.domain.com".isLikelyEmail, "Multiply dotted domain rejected.")
+    XCTAssertTrue("user@strange.top.level.domain".isLikelyEmail, "Non-.com-domain rejected.")
+    XCTAssertTrue("user123456789@domain.com".isLikelyEmail, "Alphanumeric username rejected.")
+    XCTAssertTrue("user@domain5.com".isLikelyEmail, "Alphanumeric domain rejected.")
+    XCTAssertTrue("004798765432@telia.no".isLikelyEmail, "Telephone number-like username rejected.")
   }
 
   func testInvalidEmails() {
-    XCTAssertFalse("a@a.a".isLikelyEmail, "Subminimal case.")
-    XCTAssertFalse("voting booth@domain.com".isLikelyEmail, "Username contains whitespace.")
-    XCTAssertFalse("domain.com".isLikelyEmail, "Domain only.")
-    XCTAssertFalse("user@thisdomain".isLikelyEmail, "Email address is local.")
-    XCTAssertFalse("@domain.com".isLikelyEmail, "No username.")
-    XCTAssertFalse("user@voting-booth+gmail.com".isLikelyEmail, "Domain contains plussed component.")
-    XCTAssertFalse("user".isLikelyEmail, "Username only.")
-    XCTAssertFalse("+voting-booth@domain.com".isLikelyEmail, "Username doesn't start with an alphanumeric.")
-    XCTAssertFalse("Foo user@domain.com".isLikelyEmail, "Junk before email address.")
-    XCTAssertFalse("user@domain name.com".isLikelyEmail, "Domain contains whitespace.")
-    XCTAssertFalse("Test User <testuser@unicus.com>".isLikelyEmail, "Address is complete specification.")
+    XCTAssertFalse("a@a.a".isLikelyEmail, "Subminimal case accepted.")
+    XCTAssertFalse("voting booth@domain.com".isLikelyEmail, "Username containing whitespace accepted.")
+    XCTAssertFalse("domain.com".isLikelyEmail, "Domain only accepted.")
+    XCTAssertFalse("user@thisdomain".isLikelyEmail, "Local email address accepted.")
+    XCTAssertFalse("@domain.com".isLikelyEmail, "Address without username accepted.")
+    XCTAssertFalse("user@voting-booth+gmail.com".isLikelyEmail, "Domain containing plussed compontent accepted.")
+    XCTAssertFalse("user".isLikelyEmail, "Email address with username only is accepted.")
+    XCTAssertFalse("+voting-booth@domain.com".isLikelyEmail, "Username not starting with an alphanumeric character accepted.")
+    XCTAssertFalse("Foo user@domain.com".isLikelyEmail, "Username containing whitespace accepted.")
+    XCTAssertFalse("user@domain name.com".isLikelyEmail, "Domain containing whitespace accepted.")
+    XCTAssertFalse("Test User <testuser@unicus.com>".isLikelyEmail, "Complete address specification accepted.")
   }
 
   func testCanonicalEmail() {
