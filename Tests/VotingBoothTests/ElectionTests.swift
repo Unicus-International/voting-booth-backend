a6ad343bb16da1be8e6984e2e7613d05ee3138a8
Williham Totland <williham.totland@gmail.com>
2019-10-07 08:20:21 +0200

    Reframes the test failure reasons of ElectionTests in terms of how the assertion failed.

--- Tests/VotingBoothTests/ElectionTests.swift
+++ Tests/VotingBoothTests/ElectionTests.swift
@@ -7,12 +7,12 @@ final class ElectionTests: XCTestCase {
 
     election.generateFranchises(5)
 
-    XCTAssertEqual(election.franchises.count, 5, "The correct number of franchises are generated.")
+    XCTAssertEqual(election.franchises.count, 5, "An incorrect number of franchises are generated.")
 
     election.generateFranchises(5)
 
-    XCTAssertEqual(election.franchises.count, 10, "The correct number of franchises are generated.")
-    XCTAssertEqual(election.franchiseMap.count, 10, "Franchises have different keys.")
+    XCTAssertEqual(election.franchises.count, 10, "An incorrect number of franchises are generated.")
+    XCTAssertEqual(election.franchiseMap.count, 10, "Multiple franchises have the same key.")
   }
 
   func testBallots() {
@@ -21,31 +21,31 @@ final class ElectionTests: XCTestCase {
     election.addBallot(named: "A ballot.", with: [])
     election.addBallot(named: "Another ballot.", with: [])
 
-    XCTAssertEqual(election.ballots.count, 2, "The correct number of ballots are added.")
-    XCTAssertEqual(election.ballotMap.count, 2, "Ballots have different keys.")
+    XCTAssertEqual(election.ballots.count, 2, "An incorrect number of ballots are added.")
+    XCTAssertEqual(election.ballotMap.count, 2, "Multiple ballots have the same key.")
   }
 
   func testCandidates() {
     let election = Election("Election!", question: "Question?", from: Date(), to: Date())
 
     election.addBallot(named: "A ballot.", with: Candidate(named: "A candidate."), Candidate(named: "Another candidate."))
-    XCTAssertEqual(election.candidates.count, 2, "The correct number of candidates are found.")
+    XCTAssertEqual(election.candidates.count, 2, "An incorrect number of candidates are found.")
 
     election.addBallot(named: "Another ballot.", with: Candidate(named: "A candidate."), Candidate(named: "Another candidate."))
-    XCTAssertEqual(election.candidates.count, 4, "The correct number of candidates are found.")
-    XCTAssertEqual(election.candidateMap.count, 4, "Candidates have different keys.")
+    XCTAssertEqual(election.candidates.count, 4, "An incorrect number of candidates are found.")
+    XCTAssertEqual(election.candidateMap.count, 4, "Multiple candidates have the same key.")
   }
 
   func testElectionClosedNow() {
     let election = Election("Election!", question: "Question?", from: Date(), to: Date())
 
-    XCTAssertFalse(election.isOpen, "An election closing now is already closed.")
+    XCTAssertFalse(election.isOpen, "An election closing now is still open.")
   }
 
   func testElectionClosedSoon() {
     let election = Election("Election!", question: "Question?", from: Date(), to: Date(timeIntervalSinceNow: 60.0))
 
-    XCTAssertTrue(election.isOpen, "An election closing in a minute is open.")
+    XCTAssertTrue(election.isOpen, "An election closing in a minute is already closed.")
   }
 
   func testElectionPast() {
@@ -56,7 +56,7 @@ final class ElectionTests: XCTestCase {
       to: Date(timeIntervalSinceNow: -3600.0)
     )
 
-    XCTAssertFalse(election.isOpen, "An election in the past is closed.")
+    XCTAssertFalse(election.isOpen, "An election in the past is still open.")
   }
 
   func testElectionFuture() {
@@ -67,7 +67,7 @@ final class ElectionTests: XCTestCase {
       to: Date(timeIntervalSinceNow: 7200.0)
     )
 
-    XCTAssertFalse(election.isOpen, "An election in the future is closed.")
+    XCTAssertFalse(election.isOpen, "An election in the future is already open.")
   }
 
   func testVoting() {
@@ -81,22 +81,22 @@ final class ElectionTests: XCTestCase {
     election.generateFranchises(1)
     let franchise = election.franchises.first!
 
-    XCTAssertFalse(election.hasVoted(franchise), "Vote has not been cast.")
+    XCTAssertFalse(election.hasVoted(franchise), "Vote has been cast.")
 
     let (zerothVoteCast, zerothReturns) = franchise.castVote(on: ballot, for: ballot.candidates.first!, ballot.candidates.first!)
-    XCTAssertFalse(zerothVoteCast, "Vote has not been cast.")
-    XCTAssertEqual(zerothReturns, .invalidVote, "Vote is invalid.")
+    XCTAssertFalse(zerothVoteCast, "Vote has been cast.")
+    XCTAssertEqual(zerothReturns, .invalidVote, "Vote is incorrectly labelled as valid.")
 
     let (firstVoteCast, firstReturns) = franchise.castVote(on: ballot, for: ballot.candidates.first!)
     XCTAssertTrue(firstVoteCast, "Vote has been cast.")
-    XCTAssertTrue(election.hasVoted(franchise), "Vote has been cast.")
-    XCTAssertEqual(firstReturns, .voteCast, "Vote has been cast.")
-    XCTAssertEqual(election.firstChoiceVotes(for: ballot.candidates.first!), 1, "Vote is counted.")
+    XCTAssertTrue(election.hasVoted(franchise), "Vote has not been cast.")
+    XCTAssertEqual(firstReturns, .voteCast, "Vote has not been cast.")
+    XCTAssertEqual(election.firstChoiceVotes(for: ballot.candidates.first!), 1, "Vote is not counted.")
 
     let (_, secondReturns) = franchise.castVote(on: ballot, for: ballot.candidates.last!)
-    XCTAssertEqual(secondReturns, .voteUpdated, "Vote has been updated.")
-    XCTAssertEqual(election.firstChoiceVotes(for: ballot.candidates.first!), 0, "Vote is updated.")
-    XCTAssertEqual(election.firstChoiceVotes(for: ballot.candidates.last!), 1, "Vote is counted.")
+    XCTAssertEqual(secondReturns, .voteUpdated, "Vote has not been updated.")
+    XCTAssertEqual(election.firstChoiceVotes(for: ballot.candidates.first!), 0, "Vote is not updated.")
+    XCTAssertEqual(election.firstChoiceVotes(for: ballot.candidates.last!), 1, "Vote is not counted.")
   }
 
   func testElectionNoUpdate() {
@@ -115,6 +115,6 @@ final class ElectionTests: XCTestCase {
     XCTAssertTrue(firstVoteCast, "The first vote was not accepted.")
 
     let (secondVoteCast, _) = franchise.castVote(on: ballot, for: ballot.candidates.last!)
-    XCTAssertFalse(secondVoteCast, "The second vote was accepted.")
+    XCTAssertFalse(secondVoteCast, "The second vote was erroneously accepted.")
   }
 }
